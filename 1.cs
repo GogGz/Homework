bffbfa43ea6a9638e8bd3bb2dc3a5ee205118e58
GogGz <120939822+GogGz@users.noreply.github.com>
2022-12-22 22:30:02 +0400

    Add files via upload

---
+++ 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Homework.Gohar2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t1.Enter 2 numbers and operation to proceed +, _, *,/");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+
+            switch (operation)
+            {
+                case "+" :
+                    Console.WriteLine($"a+b = {a + b}");
+                    break;
+                case "-" :
+                    Console.WriteLine($"a-b = {a - b}");
+                    break;
+                case "*" :
+                    Console.WriteLine($"a*b = {a * b}");
+                    break;
+                case "/" :
+                    Console.WriteLine($"a/b = {a / b}");
+                    break;
+                case "/%":
+                    Console.WriteLine($"a%b = {a % b}");
+                    break;
+
+
+                default:
+                    Console.WriteLine("N/D");
+                    break;
+            }
+        }
+
+    }
+
+}
